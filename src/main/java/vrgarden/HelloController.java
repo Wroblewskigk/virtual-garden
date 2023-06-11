fc152ab05cc2d73c35bb42c5ae249591e5b3528a
Wroblewskigk <wroblewskigk@gmail.com>
2023-06-11 15:14:03 +0200

    Fixed fillPaneArray()

--- src/main/java/vrgarden/HelloController.java
+++ src/main/java/vrgarden/HelloController.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 
 import java.io.OutputStream;
 import java.io.PrintStream;
@@ -25,7 +26,7 @@ public class HelloController {
     @FXML
     public GridPane gardenGrid;
 
-    public List<Node> paneArray = new ArrayList<>(100);
+    public List<Object> paneArray = new ArrayList<>(100);
 
     /**
      * A method that adds all the program's panes to the grid pane
@@ -45,8 +46,8 @@ public class HelloController {
                 numberOfIndexString = decades + unity;
                 numberOfIndex = Integer.parseInt(numberOfIndexString);
 
-                //paneArray.set(numberOfIndex, gardenGrid.getChildren().get(numberOfIndex));
-                System.out.println(numberOfIndex);
+                paneArray.add(numberOfIndex, gardenGrid.getChildren().get(numberOfIndex));
+                System.out.println(paneArray.get(numberOfIndex));
             }
         }
     }
@@ -65,22 +66,20 @@ public class HelloController {
 
         //Generate garden on button click
         Object[][] garden = GardenGenerator.GenerateGarden();
-        //changePaneColorOnField(garden);
+        changePaneColorOnField(garden);
 
         Object[][] gardenE = EntityGenerator.GenerateEntity();
 
         //Will launch example methods here, to check if they work
     }
 
-    /*
     public void changePaneColorOnField(Object[][] garden){
         for (int i=0; i<HelloApplication.GARDEN_SIZE; i++){
             for (int j=0; j<HelloApplication.GARDEN_SIZE; j++){
-                System.out.println("Debug" + garden[i][j]);
+                //System.out.println("Debug" + garden[i][j]);
             }
         }
     }
-    */
 
     public static class Console extends OutputStream {
         private final TextArea console;
